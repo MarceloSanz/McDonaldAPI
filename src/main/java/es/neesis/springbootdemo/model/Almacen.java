e25d1639e618fc2fe0a4e166308144577aba5c6e
Andrés Montaño Villafañe <andres.montano@babelgroup.com>
2024-04-12 13:19:22 +0200

    primera version

---
+++ src/main/java/es/neesis/springbootdemo/model/Almacen.java
@@ -0,0 +1,11 @@
+package es.neesis.springbootdemo.model;
+
+import es.neesis.springbootdemo.repository.IAlmacenDB;
+
+import java.util.List;
+
+public class Almacen {
+
+    Producto producto;
+
+}
---
+++ src/main/java/es/neesis/springbootdemo/repository/AlmacenDB.java
@@ -0,0 +1,54 @@
+package es.neesis.springbootdemo.repository;
+
+import java.util.ArrayList;
+import es.neesis.springbootdemo.model.Producto;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AlmacenDB implements IAlmacenDB{
+    private Map<Integer, Producto> productosAlmacenados;
+
+    @Override
+    public void inicializarAlmacen() {
+
+        productosAlmacenados = new HashMap<>();
+
+        productosAlmacenados.put(1, new Producto(1, "Big Mac", 50));
+        productosAlmacenados.put(2, new Producto(2, "Royale Deluxe", 30));
+        productosAlmacenados.put(3, new Producto(3, "Patatas Gajo", 15));
+        productosAlmacenados.put(4, new Producto(4, "Patatas Fritas", 20));
+        productosAlmacenados.put(5, new Producto(5, "McFlurry", 10));
+        productosAlmacenados.put(6, new Producto(6, "Coca-Cola", 50));
+
+    }
+
+
+    @Override
+    public List<Producto> listarProductosAlmacen() {
+        return new ArrayList<Producto>(productosAlmacenados.values());
+    }
+
+    @Override
+    public Producto productoPorId(int id) {
+        return productosAlmacenados.get(id);
+    }
+
+    @Override
+    public int cantidadProductoAlmacenado(int id) {
+        return productoPorId(id).getCantidad();
+    }
+
+    public boolean pushProducto(int id, int cantidad){
+        Producto producto = productoPorId(id);
+        producto.setCantidad(producto.getCantidad() + cantidad);
+        return true;
+    }
+
+    public boolean popProducto(int id, int cantidad){
+        Producto producto = productoPorId(id);
+        producto.setCantidad(producto.getCantidad() - cantidad);
+    }
+
+}
