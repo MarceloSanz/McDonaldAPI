76772277349f88b73c1b2fe829fab3f529909bdb
Andrés Montaño Villafañe <andres.montano@babelgroup.com>
2024-04-15 12:49:30 +0200

    Actualizacion Almacen Controller

--- src/main/java/es/neesis/springbootdemo/controllers/AlmacenController.java
+++ src/main/java/es/neesis/springbootdemo/controllers/AlmacenController.java
@@ -2,33 +2,38 @@ package es.neesis.springbootdemo.controllers;
 
 
 import es.neesis.springbootdemo.model.Producto;
-import es.neesis.springbootdemo.repository.IAlmacenDB;
 import es.neesis.springbootdemo.services.IAlmacenService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/producto")
-public class ProductoController {
+@RequestMapping(value = "/almacen")
+public class AlmacenController {
     private final IAlmacenService almacenService;
 
-    public ProductoController(IAlmacenService almacenService) {
+    public AlmacenController(IAlmacenService almacenService) {
         this.almacenService = almacenService;
     }
+
+    @PostMapping(value = "/init")
+    public void inicializarAlmacen() {
+        this.almacenService.inicializarAlmacen();
+    }
+
     @GetMapping(value = "/list")
     public List<Producto> listarTodosLosProductos(){
         return almacenService.listarProductosAlmacen();
     }
-    @GetMapping(value = "/cantidad/{id}")
-    public Producto obtenerCantidadPorID(@PathVariable int id){
+    @GetMapping(value = "/{id}")
+    public Producto obtenerProductoPorID(@PathVariable int id){
         return almacenService.productoPorId(id);
     }
-    @PostMapping(value = "/add/{id}/{cantidad}")
+    @PutMapping(value = "/add/{id}/{cantidad}")
     public boolean push(@PathVariable  int id,@PathVariable int cantidad){
         return almacenService.pushProducto(id,cantidad);
     }
-    @GetMapping(value = "/delete/{id}/{cantidad}")
+    @PostMapping(value = "/delete/{id}/{cantidad}")
     public boolean pop(@PathVariable int id, @PathVariable int cantidad){
         return almacenService.popProducto(id, cantidad);
     }
